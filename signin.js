3bbc46b89f48174108b8d9532ae5bbeec303d666
ChoEsun <88094507+ChoEsun@users.noreply.github.com>
2021-09-27 09:14:15 -0400

    signin redirect to game_start

--- signin.js
+++ signin.js
@@ -1,6 +1,5 @@
 const form = document.querySelector("form")
-let inputUname = document.querySelector(".form-signin input[type = 'email']").value
-let inputPassword = document.querySelector(".form-signin input[type = 'password']").value
+
 
 document.querySelector(".form-signin input[type = 'email']").value = localStorage.getItem("email")
 document.querySelector(".form-signin input[type = 'password']").value = localStorage.getItem("password")
@@ -10,11 +9,9 @@ localStorage.clear()
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    if (inputUname === localStorage.getItem("email") && inputPassword === localStorage.getItem("password")) {
-        window.location.href = window.location.href.replace("/signin.html", "/game_start.html")
-    } else {
-        window.location.href = window.location.href.replace("/signin.html", "/redirect.html")
-    }
+
+    window.location.href = window.location.href.replace("/signin.html", "/game_start.html")
+
 
 })
 
