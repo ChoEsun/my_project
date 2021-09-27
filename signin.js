cd569b73d299c22f0c236cefb91203d7ec17a4e0
ChoEsun <88094507+ChoEsun@users.noreply.github.com>
2021-09-27 02:36:57 -0400

    sigin validation

--- signin.js
+++ signin.js
@@ -1,7 +1,25 @@
-const sign_up_btn = document.querySelector
-('sign_up_btn');
-const login_btn = document.querySelector
-('signup-form');
-sign_up_btn.addEventListener('click', ()=>{
-    signup-form.classlist.add('sign-up-mode');
+const form = document.querySelector("form")
+let inputUname = document.querySelector(".form-signin input[type = 'email']").value
+let inputPassword = document.querySelector(".form-signin input[type = 'password']").value
+
+document.querySelector(".form-signin input[type = 'email']").value = localStorage.getItem("email")
+document.querySelector(".form-signin input[type = 'password']").value = localStorage.getItem("password")
+
+
+localStorage.clear()
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (inputUname === localStorage.getItem("email") && inputPassword === localStorage.getItem("password")) {
+        window.location.href = window.location.href.replace("/signin.html", "/game_start.html")
+    } else {
+        window.location.href = window.location.href.replace("/signin.html", "/redirect.html")
+    }
+
 })
+
+///const login_btn = document.querySelector('signup-form');
+//sign_up_btn.addEventListener('click', () => {
+//signup - form.classlist.add('sign-up-mode');
+//location.href = "/game_start.html"
+//})
